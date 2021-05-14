5460be307682ae1f0d132a3f19bcb53f0fb0e553
constmarch <kostyrnd@yandex.ru>
2021-05-15 02:00:48 +0300

    returned allgroups and grade tables

--- lib/database/gerard-benedict.dart
+++ lib/database/gerard-benedict.dart
@@ -26,6 +26,17 @@ class DBProvider {
     String path = join(documentsDirectory.path, dbname);
     return await openDatabase(path, version: 1, onOpen: (db) {},
         onCreate: (Database db, int version) async {
+      await db.execute("CREATE TABLE Grade ("
+          "id INTEGER,"
+          "num INTEGER,"
+          "degree TEXT"
+          ")");
+      await db.execute("CREATE TABLE AllGroups ("
+          "id INTEGER,"
+          "name TEXT,"
+          "num INTEGER,"
+          "gradeid INTEGER"
+          ")");
       await db.execute("CREATE TABLE Groups ("
           "updateable INTEGER,"
           "id INTEGER,"
@@ -52,6 +63,39 @@ class DBProvider {
           ")");
     });
   }
+
+  //** "New" section
+  newGroup(Group gr, int updInd) async {
+    final db = await database;
+    var json = gr.toJson();
+    json['updateable'] = updInd;
+    var res = await db.insert("Groups", json);
+    return res;
+  }
+
+  newNormalLesson(NormalLesson normalLesson, int updInd) async {
+    final db = await database;
+    var json = normalLesson.toJson();
+    json['updateable'] = updInd;
+    var res = await db.insert("NormalLesson", json);
+    return res;
+  }
+
+  newGrade(Grade newGrade) async {
+    final db = await database;
+    var res = await db.insert("Grade", newGrade.toJson());
+    return res;
+  }
+
+  newAllGroups(Group group) async {
+    final db = await database;
+    var res = await db.insert("AllGroups", group.toJson());
+    return res;
+  }
+
+  //** "New" section's end  **/
+
+  //** Primitive "get" section
 
   getNormalLesson(int grId) async {
     final db = await database;
@@ -90,117 +134,17 @@ class DBProvider {
     return db.query("Groups", where: ("groupid = $uberid"));
   }
 
-  newGroup(Group gr, int updInd) async {
+  Future<List<Group>> _getAllGroups() async {
     final db = await database;
-    var json = gr.toJson();
-    json['updateable'] = updInd;
-    var res = await db.insert("Groups", json);
-    return res;
+    var res = await db.query("AllGroups");
+    List<Group> list =
+        res.isNotEmpty ? res.map((c) => Group.fromJson(c)).toList() : [];
+    return list;
   }
 
-  newNormalLesson(NormalLesson normalLesson, int updInd) async {
-    final db = await database;
-    var json = normalLesson.toJson();
-    json['updateable'] = updInd;
-    var res = await db.insert("NormalLesson", json);
-    return res;
-  }
+  //** Primitive "get" section's end **/
 
-  refreshNormalLesson(List<NormalLesson> lessons) async {
-    final db = await database;
-    db.rawDelete("DELETE FROM NormalLesson WHERE id = ${1}");
-    await Future.forEach(lessons, (lesson) async {
-      await newNormalLesson(lesson, 1);
-    });
-  }
-
-  refreshGroup(List<Group> groups) async {
-    final db = await database;
-    db.rawDelete("DELETE FROM Groups WHERE updateable = ${1}");
-    await Future.forEach(groups, (group) async {
-      await newGroup(group, 1);
-    });
-  }
-
-  Future<List<Week>> getWeeksForStudent() async {
-    var lessons = await getNormalLessons(1);
-    if (lessons.isEmpty) {
-      return [];
-    }
-    List<Week> res = [Week.fromDB(), Week.fromDB()];
-    var lowerlessons =
-        lessons.where((lesson) => lesson.typeOfWeek == TypeOfWeek.lower);
-    var upperlessons =
-        lessons.where((lesson) => lesson.typeOfWeek == TypeOfWeek.upper);
-    for (int i = 0; i < 7; i++) {
-      var lowdaylessons =
-          lowerlessons.where((lesson) => lesson.dayid == i).toList();
-      lowdaylessons.sort(
-        (l1, l2) => IntervalOfTime.compare(l1.time, l2.time),
-      );
-
-      var updaylessons =
-          upperlessons.where((lesson) => lesson.dayid == i).toList();
-      updaylessons.sort(
-        (l1, l2) => IntervalOfTime.compare(l1.time, l2.time),
-      );
-      res[0].days[i].normalLessons = lowdaylessons;
-      res[1].days[i].normalLessons = updaylessons;
-    }
-    return res;
-  }
-
-  Future<List<Week>> getWeeksForTeacher() async {
-    var lessons = await getNormalLessons(1);
-    var groups = await getGroups(1);
-    if (lessons.isEmpty) {
-      return [];
-    }
-
-    List<Group> _deleteRepetitions(List<Group> groups) {
-      Set<String> groupsAsSet = Set<String>();
-      List<Group> res = [];
-      groups.forEach(
-        (group) {
-          if (!groupsAsSet.contains(group.asString())) {
-            groupsAsSet.add(group.asString());
-            res.add(group);
-          }
-        },
-      );
-      return res;
-    }
-
-    for (int i = 0; i < lessons.length; i++) {
-      var gr =
-          groups.where((group) => group.uberid == lessons[i].uberid).toList();
-      gr = _deleteRepetitions(gr);
-      lessons[i].groups = gr;
-    }
-
-    List<Week> res = [Week.fromDB(), Week.fromDB()];
-    var lowerlessons =
-        lessons.where((lesson) => lesson.typeOfWeek == TypeOfWeek.lower);
-    var upperlessons =
-        lessons.where((lesson) => lesson.typeOfWeek == TypeOfWeek.upper);
-    for (int i = 0; i < 7; i++) {
-      var lowdaylessons =
-          lowerlessons.where((lesson) => lesson.dayid == i).toList();
-      lowdaylessons.sort(
-        (l1, l2) => IntervalOfTime.compare(l1.time, l2.time),
-      );
-
-      var updaylessons =
-          upperlessons.where((lesson) => lesson.dayid == i).toList();
-      updaylessons.sort(
-        (l1, l2) => IntervalOfTime.compare(l1.time, l2.time),
-      );
-
-      res[0].days[i].normalLessons = lowdaylessons;
-      res[1].days[i].normalLessons = updaylessons;
-    }
-    return res;
-  }
+  //** "Refresh" section
 
   Future<bool> refreshDb() async {
     bool databaseDeleted = false;
@@ -225,6 +169,146 @@ class DBProvider {
 
     return databaseDeleted;
   }
+
+  refreshNormalLesson(List<NormalLesson> lessons) async {
+    final db = await database;
+
+    db.rawDelete("DELETE FROM NormalLesson WHERE id = ${1}");
+    await Future.forEach(lessons, (lesson) async {
+      await newNormalLesson(lesson, 1);
+    });
+  }
+
+  refreshGroup(List<Group> groups) async {
+    final db = await database;
+    db.rawDelete("DELETE FROM Groups WHERE updateable = ${1}");
+    await Future.forEach(groups, (group) async {
+      await newGroup(group, 1);
+    });
+  }
+
+  refreshWeeks(List<Week> weeks) async {
+    final db = await database;
+
+    db.rawDelete("DELETE FROM NormalLesson WHERE updateable = ${1}");
+    db.rawDelete("DELETE FROM Groups WHERE updateable = ${1}");
+    await Future.forEach(
+      weeks,
+      (week) async {
+        await Future.forEach(
+          week.days,
+          (day) async {
+            await Future.forEach(
+              day.normalLessons,
+              (normalLesson) async {
+                await newNormalLesson(normalLesson, 1);
+                var groups = normalLesson.groups;
+                if (groups != null) {
+                  Future.forEach(
+                    groups,
+                    (group) async {
+                      await newGroup(group, 1);
+                    },
+                  );
+                }
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  //** "Refresh" section's end **/
+
+  //** "Get" section
+
+  Future<List<Grade>> getAllGrades() async {
+    final db = await database;
+    var res = await db.query("Grade");
+    List<Grade> list =
+        res.isNotEmpty ? res.map((c) => Grade.fromJson(c)).toList() : [];
+    return list;
+  }
+
+  Future<List<List<Group>>> getAllGroups(List<int> gradeid) async {
+    var allgroups = await _getAllGroups();
+    List<List<Group>> res = [];
+    gradeid.forEach(
+      (id) {
+        var groups = allgroups.where((group) => group.gradeid == id).toList();
+        res.add(groups);
+      },
+    );
+    return res;
+  }
+
+  Future<List<Week>> getWeeksForStudent(int updateable) async {
+    var lessons = await getNormalLessons(updateable);
+    if (lessons.isEmpty) {
+      return [];
+    }
+    return _getWeeks(lessons);
+  }
+
+  Future<List<Week>> getWeeksForTeacher(int updateable) async {
+    var lessons = await getNormalLessons(updateable);
+    var groups = await getGroups(updateable);
+    if (lessons.isEmpty) {
+      return [];
+    }
+
+    List<Group> _deleteRepetitions(List<Group> groups) {
+      Set<String> groupsAsSet = Set<String>();
+      List<Group> res = [];
+      groups.forEach(
+        (group) {
+          if (!groupsAsSet.contains(group.asString())) {
+            groupsAsSet.add(group.asString());
+            res.add(group);
+          }
+        },
+      );
+      return res;
+    }
+
+    for (int i = 0; i < lessons.length; i++) {
+      var gr =
+          groups.where((group) => group.uberid == lessons[i].uberid).toList();
+      gr = _deleteRepetitions(gr); //TODO удалять повтори при записи
+      lessons[i].groups = gr;
+    }
+
+    return _getWeeks(lessons);
+  }
+
+  List<Week> _getWeeks(List<NormalLesson> lessons) {
+    List<Week> res = [Week.fromDB(), Week.fromDB()];
+    var lowerlessons =
+        lessons.where((lesson) => lesson.typeOfWeek == TypeOfWeek.lower);
+    var upperlessons =
+        lessons.where((lesson) => lesson.typeOfWeek == TypeOfWeek.upper);
+    for (int i = 0; i < 7; i++) {
+      var lowdaylessons =
+          lowerlessons.where((lesson) => lesson.dayid == i).toList();
+      lowdaylessons.sort(
+        (l1, l2) => IntervalOfTime.compare(l1.time, l2.time),
+      );
+
+      var updaylessons =
+          upperlessons.where((lesson) => lesson.dayid == i).toList();
+      updaylessons.sort(
+        (l1, l2) => IntervalOfTime.compare(l1.time, l2.time),
+      );
+
+      res[0].days[i].normalLessons = lowdaylessons;
+      res[1].days[i].normalLessons = updaylessons;
+    }
+    return res;
+  }
+  //** "Get" section's end
+
+  //** "Fill" section
 
   fillWeeks(List<Week> weeks) async {
     await Future.forEach(
@@ -253,30 +337,29 @@ class DBProvider {
     );
   }
 
-  refreshWeeks(List<Week> weeks) async {
-    final db = await database;
-
-    db.rawDelete("DELETE FROM NormalLesson WHERE updateable = ${1}");
-    db.rawDelete("DELETE FROM Groups WHERE updateable = ${1}");
+  fillAllGroupTable(List<List<Group>> allgroups) async {
     await Future.forEach(
-      weeks,
-      (week) async {
+      allgroups,
+      (groups) async {
         await Future.forEach(
-          week.days,
-          (day) async {
-            await Future.forEach(
-              day.normalLessons,
-              (normalLesson) async {
-                await newNormalLesson(normalLesson, 1);
-                var groups = normalLesson.groups;
-                if (groups != null) {
-                  await newGroup(normalLesson.groups, 1);
-                }
-              },
-            );
+          groups,
+          (group) async {
+            await newAllGroups(group);
           },
         );
       },
     );
   }
+
+  fillGradeTable(List<Grade> grades) async {
+    await Future.forEach(
+      grades,
+      (grade) async {
+        await newGrade(grade);
+      },
+    );
+  }
+
+  //** "Fill" section's end **/
+
 }
